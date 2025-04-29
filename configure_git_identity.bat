29b7d10c9bab6f67fbe1ac52144e73bf4772a21d
Sambot888 <sambot888@163.com>
2025-04-29 22:59:37 +0800

    auto: sync and push

---
+++ configure_git_identity.bat
@@ -0,0 +1,9 @@
+
+@echo off
+echo Setting global Git identity...
+
+git config --global user.name "Sambot888"
+git config --global user.email "sambot888@163.com"
+
+echo Done. You can now push to GitHub using your identity.
+pause
